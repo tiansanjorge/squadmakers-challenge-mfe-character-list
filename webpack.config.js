971d4b762a0efe178c6b1d409829cb5eb15d8676
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-05-11 18:33:25 -0300

    feat: webpack config para png

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
