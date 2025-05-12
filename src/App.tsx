6e1943e4438bc5e74952db68090093a9a8482cb6
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2025-05-12 04:08:37 -0300

    fix: modal crash

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import CharacterList from "./components/CharacterList";
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-50">
+      <div className=" bg-gray-50">
         <h1 className="text-3xl font-bold text-center py-6">Personajes</h1>
         <CharacterList searchText={""} resetSearch={() => {}} />
       </div>
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import { X } from "lucide-react";
+import { MoreVertical, X } from "lucide-react";
 
 type ModalProps = {
   isOpen: boolean;
@@ -11,18 +11,19 @@ type ModalProps = {
 export const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
-  return ReactDOM.createPortal(
-    <div className="fixed inset-0 z-50 bg-black bg-opacity-60 flex items-center justify-center">
-      <div className="bg-white rounded-xl w-11/12 sm:w-full sm:max-w-lg relative shadow-lg">
+  return (
+    <div className="fixed inset-0 z-50 bg-black bg-opacity-60 flex items-center justify-center sm:px-0">
+      <div className="bg-white rounded-xl w-full sm:max-w-lg relative shadow-lg max-h-[95vh] overflow-y-auto">
+        {/* Botón de cerrar y 3 puntitos visible solo en desktop */}
         <button
           onClick={onClose}
-          className="bg-white p-2 rounded-full absolute top-4 right-4 text-gray-500 hover:text-gray-700 z-10"
+          className="hidden sm:flex bg-white p-2 rounded-full absolute top-4 right-4 text-gray-500 hover:text-gray-700 z-10"
         >
           <X className="w-5 h-5" />
         </button>
+        <MoreVertical className="absolute top-[165px] right-5 w-6 h-6 text-[#808C73] cursor-pointer" />
         {children}
       </div>
-    </div>,
-    document.body
+    </div>
   );
 };
